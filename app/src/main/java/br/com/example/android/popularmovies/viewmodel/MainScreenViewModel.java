8f2819570b23c2bc43f203fa36c69b04e56e91ac
Henrique Vasconcelos <hvasconcelos@hp.com>
2016-10-05 16:34:29 -0300

    Added reactive for mock data
    Started the migration of the AsyncTasks for reactive. It worked neatly on
    mockData flavor.

--- app/src/main/java/br/com/example/android/popularmovies/viewmodel/MainScreenViewModel.java
+++ app/src/main/java/br/com/example/android/popularmovies/viewmodel/MainScreenViewModel.java
@@ -1,33 +1,41 @@
 package br.com.example.android.popularmovies.viewmodel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.example.android.popularmovies.model.data.Movie;
-import br.com.example.android.popularmovies.model.data.OnMoviesFetchedListener;
 import br.com.example.android.popularmovies.model.networking.MovieFetcher;
+import rx.Observer;
 
 public class MainScreenViewModel {
     private final DataListener listener;
+    private List<Movie> movies;
 
     public MainScreenViewModel(DataListener listener) {
         this.listener = listener;
     }
 
     public void onCreate() {
+        movies = new ArrayList<>();
         /**
          * We use flavors to determine which movie provider to use.
          * mockData flavor will return a preset list of movies, whereas realData will recover
          * movies from the movieDB API.
          */
-        MovieFetcher.getInstance().fetchMovies(new OnMoviesFetchedListener() {
+        MovieFetcher.getInstance().fetchMovies(new Observer<Movie>() {
             @Override
-            public void onMoviesFetched(List<Movie> movies) {
+            public void onCompleted() {
                 listener.updateMovies(movies);
             }
 
             @Override
-            public void onError() {
+            public void onError(Throwable e) {
                 listener.showErrorMessage();
+            }
+
+            @Override
+            public void onNext(Movie movie) {
+                movies.add(movie);
             }
         });
     }
